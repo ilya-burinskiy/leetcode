e58a6dd65330b6327c65d1255a09928cb9c7de87
Ilya Burinsky <burinskiy992@gmail.com>
2024-06-10 22:22:29 +0300

    added memoization to generatate_parenthesis

--- gen_parentheses.rb
+++ gen_parentheses.rb
@@ -1,11 +1,26 @@
 def generate_parenthesis(n)
+  @table ||= {0 => [""], 1 => ["()"]}
+
   return [""] if n == 0
   return ["()"] if n == 1
 
   res = []
   (1..n).each do |k|
-    res += combine(generate_parenthesis(k - 1), generate_parenthesis(n - k))
+    pseq1 = if @table.key?(k - 1)
+              @table[k - 1]
+            else
+              @table[k - 1] = generate_parenthesis(k - 1)
+              @table[k - 1]
+            end
+    pseq2 = if @table.key?(n - k)
+              @table[n - k]
+            else
+              @table[n - k] = generate_parenthesis(n - k)
+              @table[n - k]
+            end
+    res += combine(pseq1, pseq2)
   end
+
   res
 end
 
